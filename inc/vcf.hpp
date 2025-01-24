d4be9aae3097ab0ea913194a7b00d7df37ed06c7
Alex Mercadel <alexmercadel@Alexs-MacBook-Pro.local>
2025-01-24 13:01:50 -0600

    added overloads for the bitset implementation

--- inc/vcf.hpp
+++ inc/vcf.hpp
@@ -1,5 +1,12 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+
+
+#define MAX_N_SAMPLES 5000
+
 
 
 
@@ -7,7 +14,6 @@
 
 
 void getSiteMappingAndGenotypes(char* vcf_file, std::vector<std::vector<int>> &genotype_array, std::vector<int> &site_mapping, int n_threads);
-void getSiteMappingAndGenotypes(char* vcf_file, std::map<int, std::vector<int>> &alt_map, std::vector<int> &site_mapping, int n_threads);
+void getSiteMappingAndGenotypes(char* vcf_file, std::unordered_map<int, std::bitset<MAX_N_SAMPLES>> &alt_map, std::vector<int> &site_mapping, int n_threads);
 std::vector<char*> splitVCFByPos(char* input_vcf, std::vector<std::pair<int, int>> &overlapping_windows);
-int getAllele(int site, int hap, std::map<int, std::vector<int>> &alt_map);
-// void new_genotype_array(char* vcf_file);
+int getAllele(int site, int hap, std::unordered_map<int, std::bitset<MAX_N_SAMPLES>> &alt_map);
